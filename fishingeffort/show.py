c3c6addcc7dc92ef12169784758fab33bd449c8d
Sarah Paradis <sara.paradisvilar@gmail.com>
2025-08-21 16:36:23 +0200

    Added a function that plots trawling identification (True vs False) to assess if fishing effort was properly determined

--- fishingeffort/show.py
+++ fishingeffort/show.py
@@ -1,22 +1,27 @@
 # Functions to plot the data
 import geopandas as gpd
+import pandas as pd
 import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import norm
-from fishingeffort.utils import _print_with_time
-
-world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+from fishingeffort.utils import _print_with_time, _cartopy_UTM_projection
+import os
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+from matplotlib.colors import TwoSlopeNorm
+import math
 
 
 # Plot gaussian distribution of vessel speed
-def draw_histogram(data, speed_kwds=None, fig_name=None, format_fig='eps'):
+def draw_histogram(data, df_speed_params=None, speed_unit = 'kn', fig_name=None, format_fig='eps',
+                   dir_out=None):
     """
     Creates a histogram of a dataset. Ideally used to plot speed of fishing vessels to identify trawling speed
 
     data = series where the data is saved (usually saved under a column named "Sog")
 
-    df_speed = DataFrame where the information on the speed a trawler operates at or navigates is given.
+    df_speed_params = DataFrame where the information on the speed a trawler fishes at or navigates is given.
     If it is not specified, it will not annotate the histogram.
 
     fig_name = name of the graph to be saved as. If it is not specified, the histogram will not be saved.
@@ -32,51 +37,160 @@ def draw_histogram(data, speed_kwds=None, fig_name=None, format_fig='eps'):
         sns.set_context('notebook', font_scale=1.25)
         # Plot the figure
         ax = sns.histplot(data, kde=False, color='grey')
-        ax.set(xlabel='Speed over ground')
+        ax.set(xlabel=f'Vessel speed ({speed_unit})')
         ax.set(xlim=(0, 16))
         ax.set(ylabel='Frequency')
         y_max = max(list(h.get_height() for h in sns.histplot(data).patches))
         ax.set(ylim=(0, y_max * 1.5))
-        if speed_kwds is not None:
-            if 'avg_trawl_speed' in speed_kwds.keys():
-                avg_speed_trawling = speed_kwds['avg_speed_trawling']
-            if 'min_trawl_speed' in speed_kwds.keys():
-                min_speed_trawling = speed_kwds['min_speed_trawling']
-            # TODO finish figure
-            # # Establish parameters of trawling
-            # avg_speed_trawling = speed_kwds['']
-            # std_dev_trawling = df_speed.iloc[1, 0]
-            # max_freq_trawling = df_speed.iloc[2, 0]
-            # min_speed = round((df_speed.iloc[0, 0] - stds * df_speed.iloc[1, 0]), 1)
-            # max_speed = round((df_speed.iloc[0, 0] + stds * df_speed.iloc[1, 0]), 1)
-            #
-            # # Annotate the graph with trawling information
-            # ax.text((min_speed + std_dev_trawling * 0.5), y_max * 1.25, 'Trawling', color='blue', size=13.5)
-            # ax.text((min_speed + std_dev_trawling * 0.5), y_max * 1.1, '%.1f - %.1f kn' % (min_speed, max_speed),
-            #         color='blue', size=10)
-            #
-            # # Establish parameters of navigating
-            # avg_speed_nav = df_speed.iloc[3, 0]
-            # std_dev_nav = df_speed.iloc[4, 0]
-            # max_freq_nav = df_speed.iloc[5, 0]
-            #
-            # # Annotate the graph with navigating information
-            # ax.text((avg_speed_nav - std_dev_nav), y_max * 0.5, 'Navigating', color='black')
-            #
-            # # Plot Gaussian distribution between 0 and 8 with .0001 steps.
-            # x_axis = np.arange(0, 8, 0.0001)
-            # ax.plot(x_axis, norm.pdf(x_axis, avg_speed_trawling, std_dev_trawling) * 200000, linestyle='-',
-            #         color='blue')
-            #
-            # # Plot limits where we consider trawling speeds
-            # plt.axvline(min_speed, color='blue', linestyle='--')
-            # plt.axvline(max_speed, color='blue', linestyle='--')
+        if df_speed_params is not None:
+            color = 'firebrick'
+            # Establish parameters of trawling
+            avg_speed_trawling = df_speed_params['mean']['trawling']
+            min_speed_trawling = df_speed_params['range_lower']['trawling']
+            max_speed_trawling = df_speed_params['range_upper']['trawling']
+            std_dev_trawling = df_speed_params['std_dev']['trawling']
+
+            # Annotate the graph with trawling information
+            ax.annotate('Trawling', (avg_speed_trawling, y_max*1.25), color=color, size=13.5,
+                        va='center', ha='center')
+            ax.annotate(f'{min_speed_trawling:.1f} - {max_speed_trawling:.1f} {speed_unit}',
+                        (avg_speed_trawling, y_max*1.15), color=color, size=10,
+                        va='center', ha='center')
+
+            # Plot Gaussian distribution of trawling
+            def gauss(x, mu, sigma, A):
+                return A * np.exp(-(x - mu) ** 2 / (2 * sigma ** 2))
+
+            x_plot = np.linspace(0, max_speed_trawling*1.5, 500)
+            y_mode = gauss(x_plot, avg_speed_trawling, std_dev_trawling, y_max)  # scale for visualization
+            ax.plot(x_plot, y_mode, color=color, lw=2)
+
+            # Plot limits where we consider trawling speeds
+            ax.axvline(min_speed_trawling, color=color, linestyle='--')
+            ax.axvline(max_speed_trawling, color=color, linestyle='--')
+
+
+            # Establish parameters of navigating
+            avg_speed_nav = df_speed_params['mean']['navigating']
+            min_speed_nav = df_speed_params['range_lower']['navigating']
+            max_speed_nav = df_speed_params['range_upper']['navigating']
+
+            # Annotate the graph with navigating information
+            ax.annotate('Navigating', (avg_speed_nav, y_max * 1.25), color='k', size=13.5,
+                        va='center', ha='center')
+            ax.annotate(f'{min_speed_nav:.1f} - {max_speed_nav:.1f} {speed_unit}',
+                        (avg_speed_nav, y_max*1.15), color='k', size=10,
+                        va='center', ha='center')
+
+            if 'drifting' in df_speed_params.index:
+                avg_speed_drift = df_speed_params['mean']['drifting']
+                min_speed_drift = df_speed_params['range_lower']['drifting']
+                max_speed_drift = df_speed_params['range_upper']['drifting']
+
+                # Annotate the graph with navigating information
+                ax.annotate('Drifting', (avg_speed_drift, y_max * 1.25), color='k', size=13.5,
+                            va='center', ha='center')
+                ax.annotate(f'{min_speed_drift:.1f} - {max_speed_drift:.1f} {speed_unit}',
+                            (avg_speed_drift, y_max * 1.25), color='k', size=10,
+                            va='center', ha='center')
         fig = ax.get_figure()
         fig.tight_layout()
     if fig_name is not None:
         with _print_with_time('Saving histogram as ' + fig_name + '.' + format_fig):
             file_name = fig_name + '.' + format_fig
-            fig.savefig(file_name, format=format_fig, dpi=500)
+            if dir_out is not None:
+                fig.savefig(os.path.join(dir_out, file_name), format=format_fig, dpi=500)
+            else:
+                fig.savefig(file_name, format=format_fig, dpi=500)
 
 
 # TODO Plot fishing effort raster
+
+def fishing_identification_check(df, name_column, datetime_column, fig_name=None,
+                                 dir_out=None, format_fig='eps', n_fig=10,
+                                 latitude=None, longitude=None, input_crs='epsg:4326',
+                                 date_format=None):
+    import random
+    assert n_fig < 25, 'Maximum number of subplots to evaluate is 25'
+    df[datetime_column] = pd.to_datetime(df[datetime_column], format=date_format)
+    df['Date_day_temp'] = df[datetime_column].dt.date
+    if not isinstance(df, gpd.GeoDataFrame):
+        with _print_with_time('Converting DataFrame into a GeoDataFrame'):
+            assert all([longitude, latitude, input_crs]) is not None, \
+                f'No data provided for longitude, latitude, or input_crs'
+            gdf = gpd.GeoDataFrame(df,
+                                   geometry=gpd.points_from_xy(df[longitude], df[latitude]),
+                                   crs=input_crs)
+    else:
+        gdf = df
+    with _print_with_time('Creating figures'):
+        rows, cols = _compute_subplot_grid_size(n_fig)
+
+        utm_proj = _cartopy_UTM_projection(gdf)
+
+        fig, axs = plt.subplots(nrows=rows, ncols=cols, figsize=(cols*3, rows*3),
+                                constrained_layout=True,
+                                subplot_kw=dict(projection=utm_proj))
+        # Flatten axes to always return a 1D list-like
+        axs = axs.flatten() if isinstance(axs, (list, np.ndarray)) else [axs]
+
+        gdf.to_crs(utm_proj.proj4_init, inplace=True)
+
+        # Define extent of the maps
+        minx, miny, maxx, maxy = gdf.total_bounds
+        # buffer_x = (maxx - minx) * 0.1  # 10% of the width
+        # buffer_y = (maxy - miny) * 0.1  # 10% of the height
+        # minx -= buffer_x
+        # maxx += buffer_x
+        # miny -= buffer_y
+        # maxy += buffer_y
+
+        land = cfeature.NaturalEarthFeature('physical', 'land', '10m',
+                                            edgecolor='face', alpha=0.5,
+                                            facecolor='lightgreen')
+
+        vessel_day_combination = []
+        for ax in axs:
+            # Add general parameters
+            ax.set_extent([minx, maxx, miny, maxy], crs=utm_proj)
+            ax.add_feature(land)
+            ax.coastlines(resolution='10m', linewidth=0.5)
+            # Randomly choose n_fig different vessels and days to plot the data
+            vessel = random.choice(gdf[name_column].unique())
+            day = random.choice(gdf['Date_day_temp'].unique())
+            gdf_filter = gdf[(gdf[name_column] == vessel) & (gdf['Date_day_temp'] == day)]
+            while (vessel, day) in vessel_day_combination or gdf_filter.empty or \
+                    not (True in gdf_filter['Trawling'].unique()) and (False in gdf_filter['Trawling'].unique()):
+                vessel = random.choice(gdf[name_column].unique())
+                day = random.choice(gdf['Date_day_temp'].unique())
+                gdf_filter = gdf[(gdf[name_column] == vessel) & (gdf['Date_day_temp'] == day)]
+            gdf_filter.plot(column='Trawling', categorical=True, legend=True, ax=ax, zorder=5,
+                            # edgecolor='grey', linewidth=0.075,
+                            legend_kwds={"title": "Trawling"})
+            vessel_day_combination.append((vessel, day))
+            ax.set_title(f'Vessel {str(vessel)}\n({day.strftime("%d %B %Y")})', fontsize=14)
+
+    if fig_name is not None:
+        with _print_with_time('Saving figure as ' + fig_name + '.' + format_fig):
+            file_name = fig_name + '.' + format_fig
+            if dir_out is not None:
+                fig.savefig(os.path.join(dir_out, file_name), format=format_fig, dpi=500)
+            else:
+                fig.savefig(file_name, format=format_fig, dpi=500)
+
+
+def _compute_subplot_grid_size(n):
+    """
+    Computes the optimal number of rows and columns for n subplots.
+    Prioritizes a layout that's as square as possible, with rows ≥ columns.
+    """
+    # Start from the square root and adjust until it divides cleanly
+    best_rows = math.floor(math.sqrt(n))
+    best_cols = math.ceil(n / best_rows)
+
+    while best_rows * best_cols < n:
+        best_rows += 1
+        best_cols = math.ceil(n / best_rows)
+
+    return best_rows, best_cols
+
